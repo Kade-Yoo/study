44339fa1cfdca7c3c2eb61048288eb2171483542
YooHyungMin <yhm9207@naver.com>
2019-12-10 21:26:53 +0900

    JUnit으로 JPA Test 설정 및 테스트 완료
    - yml postgresql 설정
    - build.gradle test환경에서 사용할 dependency설정
    - Test 환경에서 JPA Configuration 및 select, insert 적용
    - JPA Repository 설정
    - JPA Model 구성 (@Builder, @Entity, @Columns, @Id)
    - WebConfig (보안 해제)

--- mini-project/src/main/java/com/example/mini/model/PostgresPracticeUser.java
+++ mini-project/src/main/java/com/example/mini/model/PostgresPracticeUser.java
@@ -1,6 +1,5 @@
 package com.example.mini.model;
 
-import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,33 +8,58 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.util.Assert;
+
+import com.sun.istack.Nullable;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "practice_user")
-public class PostgresPracticeUser implements Serializable {
-	
-	private static final long serialVersionUID = -735006240636182825L;
+@Getter
+@Setter
+// Table practice_user 를 나타내는 Annotation
+@Table(name = "users")
+// 파라미터 없는 생성자를 가진다. => 외부에서 접근이 가능하지 X
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class PostgresPracticeUser {
 	
 	@Id
+	@Column(name = "id")
+	// 기본 키를 나타내는 Annotation
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "user_id")
-	private String userId;
+	private long id;
+	
+	// user_email 컬럼을 나타내는 Annotation
+	@Column(name ="user_email")
+	@Nullable
+	private String userEmail;
 	
 	@Column(name = "user_name")
+	@Nullable
 	private String userName;
 	
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+	@Column(name = "user_id")
+	@Nullable
+	private String userId;
 	
-	public void setUserName(String userName) {
+	@Column(name = "user_pwd")
+	@Nullable
+	private String userPwd;
+	
+	@Builder
+	public PostgresPracticeUser(String userEmail, String userName, String userId, String userPwd) {
+		Assert.hasText(userEmail,"userEmail must not be empty");
+		Assert.hasText(userName,"userName must not be empty");
+		Assert.hasText(userId,"userId must not be empty");
+		Assert.hasText(userPwd,"userPwd must not be empty");
+		
+		this.userEmail = userEmail;
 		this.userName = userName;
-	}
-	
-	public String getUserId() {
-		return this.userId;
-	}
-	
-	public String getUserName() {
-		return this.userName;
+		this.userId = userId;
+		this.userPwd = userPwd;
 	}
 }
--- mini-project/src/test/java/com/example/mini/MiniProjectApplicationTests.java
+++ mini-project/src/test/java/com/example/mini/MiniProjectApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.mini;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import com.example.mini.model.PostgresPracticeUser;
+import com.example.mini.repository.PostgresPracticeRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+// SpringBootTest와 DataJpaTest를 같이 사용할 수 없음....
+//@SpringBootTest
+// JUnit 실행시 SpringRunner class라는 확장된 클래스를 사용
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Slf4j
 class MiniProjectApplicationTests {
-
+	@Autowired
+	PostgresPracticeRepository repository;
+	
+	// JPA를 통해서 data insert, select, update
 	@Test
-	void contextLoads() {
+	void jpa_practice01() {
+		// data insert
+		PostgresPracticeUser user = PostgresPracticeUser.builder()
+							.userEmail("yhm9207@naver.com")
+							.userId("yhm0726")
+							.userName("유형민")
+							.userPwd("dkagh")
+							.build();
+		repository.save(user);
+		
+		// data select
+		List<PostgresPracticeUser> users = repository.findAll();
+		
+		for(PostgresPracticeUser user2 : users) {
+			log.info("id : {}",user2.getId());
+			log.info("userEmail : {}",user2.getUserEmail());
+			log.info("userName : {}",user2.getUserName());
+			log.info("userId : {}",user2.getUserId());
+			log.info("userPwd : {}",user2.getUserPwd());
+		}
+		
+		// data update
 	}
 
 }
