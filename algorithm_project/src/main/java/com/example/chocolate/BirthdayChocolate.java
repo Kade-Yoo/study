37583c3b53505fe7232835007a56c388279bc33f
YooHyungMin <yhm9207@naver.com>
2020-01-26 22:14:04 +0900

    Weekend 04 - BirthdayChocolate Frist Refactoring
    - 합 계산, 조건 비교 함수 분리
    - 주석 추가

--- algorithm_project/src/main/java/com/example/chocolate/BirthdayChocolate.java
+++ algorithm_project/src/main/java/com/example/chocolate/BirthdayChocolate.java
@@ -18,6 +18,7 @@ public class BirthdayChocolate {
 	private int month;
 	private int day;
 	
+	// 초기화
 	public BirthdayChocolate(ArrayList<Integer> chocolateSegments, int month, int day) {
 		this.chocolateSegments = chocolateSegments;
 		this.month = month;
@@ -25,26 +26,39 @@ public class BirthdayChocolate {
 	}
 	
 	/**
-	 * 합 계산 후 카운트 증가
+	 * sum == day 일 때 카운트 증가
 	 * 
 	 * @return 더한 값이 day인 횟수
 	 */
 	public int calculateChocolateSegments() {
-		int sum = ZERO;
 		int count = ZERO;
 		
+		// sum 초기화 하는 로직을 줄이기 위해 for문을 각 함수로 나누었다.
 		for (int firstIndex = ZERO; this.month + firstIndex <= chocolateSegments.size(); firstIndex++) {
-			sum = ZERO;
-
-			for (int secondIndex = firstIndex; secondIndex < firstIndex + this.month; secondIndex++ ) {
-				sum = sum + chocolateSegments.get(secondIndex);
-			}
-			
-			if (sum == day) {
+			if (calculateSecondCondition(firstIndex) == day) {
 				count = count + 1;
 			}
 		}
 		
 		return count;
+	}
+	
+	/**
+	 * 합 계산 
+	 * ( 0 ~ 0 + month , 1 ~ 1 + month , 2 ~ 2 + month .... n + n + month )
+	 * segemnts.get(0) ... + segemnts.get(0 + month) 
+	 * , segemnts.get(1) ... + segemnts.get(1 + month)
+	 * , segemnts.get(n) ... + segemnts.get(n + month) 
+	 * 
+	 * @return 초콜릿 segment를 더한 값
+	 */
+	private int calculateSecondCondition(int firstIndex) {
+		int sum = ZERO;
+		
+		for(int secondIndex = firstIndex; secondIndex < firstIndex + this.month; secondIndex++) {
+			sum = sum + chocolateSegments.get(secondIndex);
+		}
+		
+		return sum;
 	}
 }
